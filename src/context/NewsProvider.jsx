1d601e39495532592b6380ee354e33175d2a10d3
Gonzalo <gonza.santi@yahoo.es>
2023-05-05 13:17:59 -0400

    Update NewsProvider.jsx
    test api url to prevent cors error

--- src/context/NewsProvider.jsx
+++ src/context/NewsProvider.jsx
@@ -11,7 +11,7 @@ export const NewsProvider = ({children}) =>{
 
     useEffect(()=>{
         const getApi= async()=>{
-            const url=`${import.meta.env.VITE_API_URL}${category}${import.meta.env.VITE_API_KEY}`;
+            const url=`https://newsapi.org/v2/top-headlines?country=us&category=${category}&apiKey=459b43d93d99418eb58d0a19d652f395`;
             const {data}= await axios(url);
             setNews(data.articles);
             setTotalNews(data.totalResults);
@@ -22,7 +22,7 @@ export const NewsProvider = ({children}) =>{
 
     useEffect(()=>{
         const getApi= async()=>{
-            const url=`${import.meta.env.VITE_API_URL}${category}&page=${page}${import.meta.env.VITE_API_KEY}`;
+            const url=`https://newsapi.org/v2/top-headlines?country=us&category=${category}&page=${page}&apiKey=459b43d93d99418eb58d0a19d652f395`;
             const {data}= await axios(url);
             setNews(data.articles);
             setTotalNews(data.totalResults);
